6cd4d512c1b2c7ddd2eb73c63f668d834c57b65a
Ortega.Bobadilla.Actividad3 <stephannie.bobadilla@alumnos.udg.mx>
2019-09-11 09:28:52 -0500

    App para sumar 2 numeros enteros

---
+++ app/src/main/java/stephannie/bobadilla/MainActivity.java
@@ -0,0 +1,39 @@
+package stephannie.bobadilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button suma = (Button)findViewById(R.id.btncalcular);
+        suma.setOnClickListener(OkListener);
+    }
+
+    private View.OnClickListener OkListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            calcularSuma();
+        }
+    };
+
+    public void calcularSuma(){
+        EditText num1 = (EditText)findViewById(R.id.num1);
+        EditText num2 = (EditText)findViewById(R.id.num2);
+        int intNum1 = Integer.parseInt(num1.getText().toString());
+        int intNum2 = Integer.parseInt(num2.getText().toString());
+        int totalSuma = intNum1 + intNum2;
+        Toast.makeText(this, "El resultado de la suma: " + totalSuma,Toast.LENGTH_LONG).show();
+    }
+}
+
+
